1f5c7b278d81ce7f2323ed378f7429bedd1fa73b
slocke <stefan.locke@etudiant.univ-rennes1.fr>
2022-03-19 16:56:41 +0100

    TESTING CHANGES

--- front/src/app/poll-service.service.ts
+++ front/src/app/poll-service.service.ts
@@ -11,7 +11,7 @@ export class PollService {
   constructor(private http: HttpClient) { }
 
   public createPoll(p: Poll): Observable<Poll> {
-    console.log('create poll');
+    console.log('create poll test change');
     return this.http.post<Poll>('/api/polls', p);
   }
 
